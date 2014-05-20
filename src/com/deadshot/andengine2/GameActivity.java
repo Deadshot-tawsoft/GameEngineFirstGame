3a1f4a50c551894bfbb50179bd70529dcb4bdd0c
tawaasalage <tawaasalage@gmail.com>
2014-05-20 23:32:35 +0530

    continues rotation added

--- src/com/deadshot/andengine2/GameActivity.java
+++ src/com/deadshot/andengine2/GameActivity.java
@@ -4,6 +4,10 @@ import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.modifier.LoopEntityModifier;
+import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.entity.modifier.ParallelEntityModifier;
+import org.andengine.entity.modifier.RotationModifier;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
@@ -18,35 +22,46 @@ import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.util.color.Color;
 
 import android.hardware.SensorManager;
+import android.widget.Toast;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-
-
 public class GameActivity extends BaseGameActivity {
 
-	Scene scene;//act as a screen
-	
-	//camera screen size
-	protected static final int CAMERA_WIDTH=800;
-	protected static final int CAMERA_HEIGHT=480;
-	
-	//player texture and its region
+	Scene scene;// act as a screen
+
+	// camera screen size
+	protected static final int CAMERA_WIDTH = 800;
+	protected static final int CAMERA_HEIGHT = 480;
+
+	// player texture and its region
 	BitmapTextureAtlas playerTexture;
 	ITextureRegion playerTextureRegion;
-	
-	//For this we apply real life physics
+
+	// For this we apply real life physics
 	PhysicsWorld physicsWorld;
-	
+
+	int angle = 0;
+
+	Sprite sPlayer;
+
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		// TODO Auto-generated method stub
-		
-		Camera mCamera=new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT); //set the camera
-		EngineOptions options=new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
+
+		Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT); // set
+																		// the
+																		// camera
+		EngineOptions options = new EngineOptions(true,
+				ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(
+						CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
+
+		Toast.makeText(getApplicationContext(), "Created", Toast.LENGTH_SHORT)
+				.show();
+
 		return options;
 	}
 
@@ -55,20 +70,22 @@ public class GameActivity extends BaseGameActivity {
 			OnCreateResourcesCallback pOnCreateResourcesCallback)
 			throws Exception {
 		// TODO Auto-generated method stub
-		
+
 		loadGfx();
-		//loading of resources finished
+		// loading of resources finished
+
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
-		
+
 	}
 
 	private void loadGfx() {
 		// TODO Auto-generated method stub
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
-		
-		//width and height should be power of two
-		playerTexture=new BitmapTextureAtlas(getTextureManager(), 64, 64);
-		playerTextureRegion=new BitmapTextureAtlasTextureRegionFactory().createFromAsset(playerTexture, this, "player.png",0,0);
+
+		// width and height should be power of two
+		playerTexture = new BitmapTextureAtlas(getTextureManager(), 64, 64);
+		playerTextureRegion = new BitmapTextureAtlasTextureRegionFactory()
+				.createFromAsset(playerTexture, this, "player.png", 0, 0);
 		playerTexture.load();
 	}
 
@@ -76,25 +93,30 @@ public class GameActivity extends BaseGameActivity {
 	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
 			throws Exception {
 		// TODO Auto-generated method stub
-		
-		this.scene=new Scene();
-		
-		this.scene.setBackground(new Background(0,125,58));
-		
-		physicsWorld=new PhysicsWorld(new Vector2(0,SensorManager.GRAVITY_EARTH), false);
+
+		this.scene = new Scene();
+
+		this.scene.setBackground(new Background(0, 125, 58));
+
+		physicsWorld = new PhysicsWorld(new Vector2(0,
+				SensorManager.GRAVITY_EARTH), false);
 		this.scene.registerUpdateHandler(physicsWorld);
-		
+
 		createWalls();
-		
+
 		pOnCreateSceneCallback.onCreateSceneFinished(this.scene);
 	}
 
 	private void createWalls() {
 		// TODO Auto-generated method stub
-		FixtureDef WALL_FIX=PhysicsFactory.createFixtureDef(0.0f, 0.0f, 0.0f);
-		Rectangle ground=new Rectangle(0, CAMERA_HEIGHT-15, CAMERA_WIDTH, 15, this.mEngine.getVertexBufferObjectManager());
-		ground.setColor(new Color(15,50,0));
-		PhysicsFactory.createBoxBody(physicsWorld, ground, BodyType.StaticBody,WALL_FIX);
+
+		FixtureDef WALL_FIX = PhysicsFactory.createFixtureDef(0.0f, 0.0f, 0.0f);
+		Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 15, CAMERA_WIDTH,
+				15, this.mEngine.getVertexBufferObjectManager());
+		ground.setColor(new Color(15, 50, 0));
+		PhysicsFactory.createBoxBody(physicsWorld, ground, BodyType.StaticBody,
+				WALL_FIX);
+
 		this.scene.attachChild(ground);
 	}
 
@@ -103,8 +125,16 @@ public class GameActivity extends BaseGameActivity {
 			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
 		// TODO Auto-generated method stub
 		
+		
+		
 		Sprite sPlayer=new Sprite(CAMERA_WIDTH/2, CAMERA_HEIGHT/2, playerTextureRegion, this.mEngine.getVertexBufferObjectManager());
-		sPlayer.setRed(45.0f);
+	
+		//sPlayer.setRotation(45.0f);
+		
+		//continuesly rotating 
+		sPlayer.registerEntityModifier(new LoopEntityModifier(new ParallelEntityModifier (new RotationModifier(6f,0f,360f), new MoveModifier(0,0,0,0,0))));
+	
+		
 		
 		final FixtureDef PLAYER_FIX=PhysicsFactory.createFixtureDef(10.0f, 1.0f, 0.0f);
 	
@@ -113,7 +143,7 @@ public class GameActivity extends BaseGameActivity {
 		this.scene.attachChild(sPlayer);
 		
 		physicsWorld.registerPhysicsConnector(new PhysicsConnector(sPlayer, body, true, false));
+		
 		pOnPopulateSceneCallback.onPopulateSceneFinished();
 	}
-
 }
